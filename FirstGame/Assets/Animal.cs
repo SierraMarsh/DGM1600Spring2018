92f6d829e529637f2142fea8eabc8a86cb28d52c
SierraMarsh <ierr.mar@gmail.com>
2018-01-18 11:15:20 -0700

    Worked on Animal Class.  Made cube say Meow.  It didn't like it

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	//this declares what start will do
+	void Start () {
+		print("my animal is eating");
+		print("now my animal is sleeping.");
+		print("I love my cat");
+		print("no I really don't");
+	}
+	void OnMouseDown()
+	{
+	  print("meow");
+	  print("Don't touch me");	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
