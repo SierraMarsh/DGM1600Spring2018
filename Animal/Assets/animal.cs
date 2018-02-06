cc76bbb9dfa47c608d895fecf94ae47358f41fd2
SierraMarsh <ierr.mar@gmail.com>
2018-02-06 10:53:32 -0700

    Extra

---
+++ Animal/Assets/animal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class animal : MonoBehaviour {
+
+public string Name;
+
+public int Foodcount;
+
+public Text myUItext
+void OnMouseDown()
+{
+	print (Name);
+	print (Foodcount);
+	Color (animal);
+}
+}
