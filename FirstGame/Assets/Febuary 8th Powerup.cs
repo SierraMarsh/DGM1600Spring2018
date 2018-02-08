73d8ba00c79a22827fd79eaa7af0ae2a8e37ab28
SierraMarsh <ierr.mar@gmail.com>
2018-02-08 10:53:49 -0700

    In class Math Operators feb 8th
    Math operators and scriptable objects

---
+++ FirstGame/Assets/Febuary 8th Powerup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Febuary8thPowerup : ScriptableObject {
+
+public int Powerlevel = 10;
+public void RunPowerUP ()
+{
+//Adds Value 
+Debug.Log(Powerlevel);
+}
+
+
+
+
+}
