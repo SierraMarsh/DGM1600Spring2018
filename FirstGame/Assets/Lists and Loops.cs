90711db925324d13dbd7f9132fddf02ad55148ec
SierraMarsh <ierr.mar@gmail.com>
2018-02-15 11:19:33 -0700

    Lists and Arrays Class Feb 15th
    To do Arrays and Lists

---
+++ FirstGame/Assets/Lists and Loops.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.ui;
+
+public class ListsandLoops : MonoBehaviour {
+public TheIncredibles TheIncredibles;
+public Text AddMember;
+
+private void OnMouseDown()
+{
+	TheIncredibles.FamilyList.Add(AddMember.Text);
+}
+
+
+}
