8a0d2a8b85b5a2740c457e0f3e9bd0b7d8e285ab
SierraMarsh <ierr.mar@gmail.com>
2018-04-12 11:26:10 -0600

    HealthBar

--- FirstGame/Assets/CharacterControler/Trigger.cs
+++ FirstGame/Assets/CharacterControler/Trigger.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour {
+	public float PowerUp = 0.2
+	public Image HealthBar
 
 	public Powerup Powerup;
 	private void OnTriggerEnter(Collider obj)
+	{ 
+		if (HealthBar.FillAmount += PowerUp)
+		if (HealthBar.FillAmount > 0)}
+
 	{
         obj.GetComponent<MoveCharacter>().MovePattern = Powerup.MovePattern;
 		gameObject.SetActive(false);
