26b05e2a55135a99dfa0e7c098290bac6b37f9cb
SierraMarsh <ierr.mar@gmail.com>
2018-04-17 11:14:18 -0600

    Class Tax Day

--- FirstGame/Assets/CharacterControler/Trigger.cs
+++ FirstGame/Assets/CharacterControler/Trigger.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Trigger : MonoBehaviour {
-	public float PowerUp = 0.2
-	public Image HealthBar
+	public float PowerUp = 0.2;
+	public Image HealthBar;
 
 	public Powerup Powerup;
 	private void OnTriggerEnter(Collider obj)
 	{ 
-		if (HealthBar.FillAmount += PowerUp)
-		if (HealthBar.FillAmount > 0)}
+		if (HealthBar.fillAmount += PowerUp)
+		if (HealthBar.fillAmount > 0)
+		
 
-	{
+	
         obj.GetComponent<MoveCharacter>().MovePattern = Powerup.MovePattern;
 		gameObject.SetActive(false);
+	
 	}
-
+	
 }
